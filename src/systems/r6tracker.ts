91edb48d6a7e4ed49caeabdf9c7cca8595d6659b
needhamgary <garyneedham1@gmail.com>
2025-02-19 13:51:28 -0600

    feat: implement R6 presence tracking and channel management

---
+++ src/systems/r6tracker.ts
@@ -0,0 +1,18 @@
+import { Collection } from "discord.js";
+
+export default class Tracker {
+  public r6Collection: Collection<string, number>;
+  public siegePlayersCount: number;
+  public uniqueMembers: Set<string>;
+  public vcChannelId: string;
+  public parentChannelId: string;
+
+  constructor() {
+    this.r6Collection = new Collection<string, number>();
+    this.siegePlayersCount = 0;
+    this.uniqueMembers = new Set<string>();
+    this.vcChannelId = "";
+    this.parentChannelId = "";
+  }
+}
+export type tracker = typeof Tracker;
--- src/utils/r6tracker.ts
+++
@@ -1,5 +0,0 @@
-import { Services } from "@sern/handler";
-
-export const r6tracker = () => {
-  const [client, logger] = Services("@sern/client", "@sern/logger");
-};
