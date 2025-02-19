bc7092a304c364de08fb0f390fec05596edc5338
needhamgary <garyneedham1@gmail.com>
2025-02-19 13:53:32 -0600

    feat: enhance Prisma connection with optional database generation and push commands

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -6,31 +6,37 @@ class Prisma extends PrismaClient {
   constructor() {
     super();
   }
+
   override async $disconnect() {
     await super
       .$disconnect()
       .then(() => logger.warn("Disconnected from Prisma ORM!"))
       .catch((e) => logger.error(e));
   }
+
   override async $connect() {
-    // exec('npx prisma generate', (err, stdout, stderr) => {
-    // 	if (err) {
-    // 		logger.error(err);
-    // 		return;
-    // 	}
-    // 	logger.info(stdout);
-    // });
+    const args = process.argv.slice(2);
+    const shouldGenerate = args.includes("--dbgen"); // node . --dbgen
+    const shouldPush = args.includes("--dbpush"); // node . --dbpush
+
+    if (shouldGenerate) {
+      exec("npx prisma generate", (err, stdout) => {
+        if (err) return logger.error(err);
+        logger.info(stdout);
+      });
+    }
+
     await super
       .$connect()
       .then(() => logger.info("Connected to Prisma ORM"))
       .catch(handlePrismaError);
-    // exec('npx prisma db push', (err, stdout, stderr) => {
-    // 	if (err) {
-    // 		logger.error(err);
-    // 		return;
-    // 	}
-    // 	logger.info(stdout);
-    // });
+
+    if (shouldPush) {
+      exec("npx prisma db push", (err, stdout) => {
+        if (err) return logger.error(err);
+        logger.info(stdout);
+      });
+    }
   }
 }
 
