5c8347b30d1c53ff184a6828a14ab43e5e8993fa
needhamgary <garyneedham1@gmail.com>
2026-01-05 18:34:09 -0600

    add global type RawResult for prisma raws

--- src/dependencies.d.ts
+++ src/dependencies.d.ts
@@ -4,11 +4,6 @@
  * Service(s) api rely on this file to provide a better developer experience.
  */
 
-import type { CoreDependencies } from "@sern/handler";
-import type { Client } from "discord.js";
-import type { Publisher } from "@sern/publisher";
-import type { Logger } from "winston";
-import type { PrismaClient } from "@prisma/client";
 import type {
   AutoRole,
   Counting,
@@ -18,6 +13,11 @@ import type {
   Tickets,
   Welcome,
 } from "#systems";
+import type { PrismaClient } from "@prisma/client";
+import type { CoreDependencies } from "@sern/handler";
+import type { Publisher } from "@sern/publisher";
+import type { Client } from "discord.js";
+import type { Logger } from "winston";
 /**
  * Note: You usually would not need to modify this unless there is an urgent need to break the contracts provided.
  * You would need to modify this to add your custom Services, however.
@@ -39,6 +39,27 @@ declare global {
     };
     process: NodeJS.Process;
   }
+  type RawResult = {
+    cursor: {
+      firstBatch: Array<{
+        _id: string;
+        name: string;
+        systems: Array<{
+          name: string;
+          enabled: boolean;
+          channels: Array<{
+            id: string;
+            messages: Array<{
+              id: string;
+            }>;
+          }>;
+        }>;
+      }>;
+      id: number;
+      ns: string;
+    };
+    ok: number;
+  };
 }
 
 export {};
