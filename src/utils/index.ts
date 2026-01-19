a702905f1a4f5aa4e080f914cbeefaee2313b754
needhamgary <garyneedham1@gmail.com>
2026-01-19 02:58:01 -0600

    feature: add translator

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,8 @@
-export * from "./prisma.js";
-export * from "./logger.js";
-export * from "./prismaErrors.js";
-export * from "./functions.js";
-export * from "./timestamp.js";
 export * from "../presence.js";
+export * from "./functions.js";
+export * from "./logger.js";
+export * from "./prisma.js";
+export * from "./prismaErrors.js";
+export * from "./timestamp.js";
+export * from "./translator.js";
+
---
+++ src/utils/translator.ts
@@ -0,0 +1,10 @@
+import { Translate } from '@google-cloud/translate/build/src/v2/index.js';
+
+const translate = new Translate({ key: process.env.GOOGLE_API_KEY });
+
+async function translateText(text: string, target: string) {
+    const [translation] = await translate.translate(text, target);
+    return translation;
+}
+
+export { translateText };
