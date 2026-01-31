f7482bc85bbdb7f0af3966eff7f0a2ea42d6f83a
needhamgary <garyneedham1@gmail.com>
2026-01-31 15:09:55 -0600

    fix: staff-role updater

--- src/components/menus/tickets/staff-role.ts
+++ src/components/menus/tickets/staff-role.ts
@@ -1,4 +1,4 @@
-import { findSystem } from "#utils";
+import { prisma } from "#utils";
 import { commandModule, CommandType } from "@sern/handler";
 import { MessageFlags, TextChannel } from "discord.js";
 
@@ -9,42 +9,31 @@ export default commandModule({
     const db = deps["@prisma/client"];
     const chan = ctx.channel as TextChannel;
     const roleId = ctx.values[0];
-    const exists = await findSystem(db.systems, chan.guild.id, "tickets");
-    if (exists) {
-      const systemIndex = exists.channels.findIndex(
-        (s) => s.name === "tickets",
-      );
-      if (systemIndex !== -1) {
-        const channelIndex = exists.channels.findIndex((c) => c.id === chan.id);
-        if (channelIndex !== -1) {
-          exists.channels[channelIndex].staffId = roleId;
-          await db.systems.update({
-            where: { id: chan.guild.id },
+    await prisma.systems.update({
+      where: { id: chan.guild.id, systems: { some: { name: "tickets" } } },
+      data: {
+        systems: {
+          updateMany: {
+            where: { name: "tickets" },
             data: {
-              systems: {
+              channels: {
                 updateMany: {
-                  where: { name: "tickets" },
-                  data: {
-                    channels: {
-                      updateMany: {
-                        where: { id: chan.id },
-                        data: { staffId: roleId },
-                      },
-                    },
-                  },
+                  where: { id: chan.id },
+                  data: { staffId: roleId },
                 },
               },
             },
-          });
-          return ctx.editReply({
-            content: `Staff role for tickets in this channel has been set to <@&${roleId}>.`,
-          });
-        }
+          },
+        },
       }
+    })
+    return ctx.editReply({
+      content: `Staff role for tickets in this channel has been set to <@&${roleId}>.`,
+    }).catch(() => {
       return ctx.editReply({
         content:
           "Could not find the ticket system configuration for this channel.",
       });
-    }
-  },
+    })
+  }
 });
