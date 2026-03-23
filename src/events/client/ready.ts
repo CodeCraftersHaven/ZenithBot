85cd6543cea9f3fe663a11e1d66faaacec62eef5
needhamgary <garyneedham1@gmail.com>
2026-03-23 10:57:25 +0000

    refactor: fix event to run all checks

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -21,34 +21,35 @@ export default eventModule({
     await syncDatabase(logger, prisma, c);
 
     const db = await prisma.giveaway.findMany();
-    if (!db || !db.length || db.length < 1) return;
-    const giveaway = new Giveaway(true);
-    const now = Date.now();
-    for (const data of db) {
-      if (data) {
-        try {
-          const endsAt = new Date(data.endsAt).getTime();
-          const timeLeft = endsAt - now;
+    if (db && db.length > 0) {
+      const giveaway = new Giveaway(true);
+      const now = Date.now();
+      for (const data of db) {
+        if (data) {
+          try {
+            const endsAt = new Date(data.endsAt).getTime();
+            const timeLeft = endsAt - now;
 
-          const channel = (await client.channels.fetch(
-            data.channelId,
-          )) as TextChannel;
-          await channel.messages.fetch(data.messageId);
-          await giveaway.createTimers(timeLeft, {
-            channelId: data.channelId,
-            messageId: data.messageId,
-            host: data.host,
-            prize: data.prize,
-            winnerSize: data.winnerSize,
-            interval: data.interval,
-          });
-        } catch (error) {
-          logger.error(
-            `Failed to fetch message with ID ${data.messageId}: ${error}: Deleting from Database...`,
-          );
-          await prisma.giveaway.deleteMany({
-            where: { messageId: data.messageId },
-          });
+            const channel = (await client.channels.fetch(
+              data.channelId,
+            )) as TextChannel;
+            await channel.messages.fetch(data.messageId);
+            await giveaway.createTimers(timeLeft, {
+              channelId: data.channelId,
+              messageId: data.messageId,
+              host: data.host,
+              prize: data.prize,
+              winnerSize: data.winnerSize,
+              interval: data.interval,
+            });
+          } catch (error) {
+            logger.error(
+              `Failed to fetch message with ID ${data.messageId}: ${error}: Deleting from Database...`,
+            );
+            await prisma.giveaway.deleteMany({
+              where: { messageId: data.messageId },
+            });
+          }
         }
       }
     }
