e217a9451607a4f3b64185bc59973b65de2483d4
needhamgary <garyneedham1@gmail.com>
2026-03-23 11:43:43 +0000

    update api logging

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,45 +3,54 @@ import cookie from "@fastify/cookie";
 import cors from "@fastify/cors";
 import { Client } from "discord.js";
 import Fastify, { FastifyInstance } from "fastify";
+import { createLogger, format, transports } from "winston";
 import router from "./routes/index.js";
 
 export const startApi = async (client: Client) => {
-  const fastify: FastifyInstance = Fastify({
-    logger: true, // Fastify has built-in high-performance logging (Pino)
-    routerOptions: {
-        ignoreTrailingSlash: true
-    }
-  });
+    const fastify: FastifyInstance = Fastify({
+        disableRequestLogging: true,
+        routerOptions: {
+            ignoreTrailingSlash: true,
+        },
+    });
 
-  await fastify.register(cookie, {
-    secret: process.env.COOKIE_SECRET || "a-very-secret-key",
-    parseOptions: {},
-  });
+    await fastify.register(cookie, {
+        secret: process.env.COOKIE_SECRET || "a-very-secret-key",
+        parseOptions: {},
+    });
 
-  await fastify.register(cors, {
-    origin: "https://zenith-bot.xyz",
-    credentials: true,
-  });
-  fastify.decorate("prisma", prisma);
-  fastify.decorate("logger", logger)
-  // Root level health check
-  fastify.get("/health", async () => {
-    return { status: "ok" };
-  });
+    await fastify.register(cors, {
+        origin: "https://zenith-bot.xyz",
+        credentials: true,
+    });
 
-  // Register all routes with /api/v1 prefix
-  await fastify.register(router, { prefix: "/api/v1", client });
+    fastify.decorate("prisma", prisma);
+    fastify.decorate("logger", logger);
 
-  const start = async () => {
-    try {
-      await fastify.ready();
-      await fastify.listen({ port: 20000, host: "0.0.0.0" });
-      fastify.logger.info("Fastify API is running on port 20000");
-    } catch (err) {
-      fastify.log.error(err);
-      process.exit(1);
-    }
-  };
+    // Root level health check
+    fastify.get("/health", async () => {
+        return { status: "ok" };
+    });
 
-  start();
+    // Register all routes with /api/v1 prefix
+    await fastify.register(router, { prefix: "/api/v1", client });
+
+    const start = async () => {
+        try {
+            await fastify.ready();
+            await fastify.listen({ port: 20000, host: "0.0.0.0" });
+            logger.info("Fastify API is running on port 20000");
+        } catch (err) {
+            logger.error(`Failed to start API: ${err}`);
+            process.exit(1);
+        }
+    };
+
+    start();
 };
+
+const win = createLogger({
+    levels: { fatal: 0, error: 1, warn: 2, info: 3, debug: 4, trace: 5, silent: 6 },
+    format: format.combine(format.splat(), format.timestamp(), format.json()),
+    transports: new transports.Console(),
+  });
